6aca6621e4deaee4291b365b13b775608aadc9b3
saurabh29-11 <saurabh97219974@gmail.com>
2026-01-23 16:24:03 +0530

    Delete Backend_Dev-class-prractice directory

--- Backend_Dev-class-prractice/16-Jan/assignment/ex5.js
+++
@@ -1,8 +0,0 @@
-console.log('A');
-
-process.nextTick(() => console.log('B'));
-Promise.resolve().then(() => console.log('C'));
-setTimeout(() => console.log('D'), 0);
-setImmediate(() => console.log('E'));
-
-console.log('F');
